3f4426b1f538e34ee71759808f24471ec81207b9
Slava Petrov <slavik--petrov@yandex.ru>
2016-05-24 13:18:00 +0300

    Add webpack support

---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,18 @@
+import Greet from './greet.jsx';
+
+class Main extends React.Component {
+	render() {
+		return (
+			<Greet />
+		);
+	}
+}
+
+let documentReady = () => {
+	ReactDOM.render(
+		<Main />,
+		document.getElementById('react')
+	);
+};
+
+$(documentReady);
