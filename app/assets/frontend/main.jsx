f082cf687bc16d63d60da0bc593d6c135266cd3b
Slava Petrov <slavik--petrov@yandex.ru>
2016-05-27 12:42:36 +0300

    Add react-router

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -2,10 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Index from "./components/Index.jsx"
 
+import { Router, Route, Link, hashHistory } from 'react-router'
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				{this.props.children}
+			</div>
+		);		
+	}
+}
+
 let documentReady = () => {
 	let reactNode = document.getElementById('react')
 	if (reactNode) {
-		ReactDOM.render(<Index />, reactNode);
+		ReactDOM.render(
+			<Router  history={hashHistory}>
+				<Route component={App}>
+					<Route path="/" component={Index} />
+				</Route>				
+				
+			</Router>
+			, reactNode);
 	}	
 };
 
